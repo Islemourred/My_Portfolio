6288c5c57ed2f3a51e1618aefeb5246295a7aca1
islemourred <ic.ourred@esi-sba.dz>
2025-08-02 04:56:27 +0100

    add achievments

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -91,7 +91,9 @@ const Contact = () => {
               </div>
             </label>
             <label className="flex flex-col">
-              <span className="text-white-100 font-medium mb-4">Your email</span>
+              <span className="text-white-100 font-medium mb-4">
+                Your email
+              </span>
               <div className="rounded-[12px] p-[1.5px] focus-within:green-pink-gradient bg-transparent transition-all duration-300">
                 <input
                   type="email"
@@ -104,7 +106,9 @@ const Contact = () => {
               </div>
             </label>
             <label className="flex flex-col">
-              <span className="text-white-100 font-medium mb-4">Your Message</span>
+              <span className="text-white-100 font-medium mb-4">
+                Your Message
+              </span>
               <div className="rounded-[12px] p-[1.5px] focus-within:green-pink-gradient bg-transparent transition-all duration-300">
                 <textarea
                   rows={7}
@@ -124,15 +128,25 @@ const Contact = () => {
                 {loading ? (
                   <>
                     <svg className="animate-spin h-5 w-5" viewBox="0 0 24 24">
-                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none"></circle>
-                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                        fill="none"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
                     </svg>
                     Sending...
                   </>
                 ) : (
-                  <>
-                    Send
-                  </>
+                  <>Send</>
                 )}
               </button>
             </div>
